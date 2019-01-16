bf9c461e7c4ce8b4a5d12562b54ed720bb8102da
Jolly Lam <jolly.lam@gmail.com>
2019-01-16 16:14:54 -0500

    First commit

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne1',
+	username: 'LC1',
+	email: 'leanne.c1@gmail.com'
+},
+{
+	id: 2,
+	name: 'Leanne2',
+	username: 'LC2',
+	email: 'leanne.c2@gmail.com'
+},
+{
+	id: 3,
+	name: 'Leanne3',
+	username: 'LC3',
+	email: 'leanne.c3@gmail.com'
+},
+{
+	id: 4,
+	name: 'Leanne4',
+	username: 'LC4',
+	email: 'leanne.c4@gmail.com'
+},
+{
+	id: 5,
+	name: 'Leanne5',
+	username: 'LC5',
+	email: 'leanne.c5@gmail.com'
+},
+]
